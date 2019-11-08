86448394fd7b2c74c43836a6f2fbde55eaf537e9
sp_res <81900409@qq.com>
2019-11-08 23:10:10 +0800

    change1

--- FastDevelop/src/main/java/org/sp/StringRange.java
+++ FastDevelop/src/main/java/org/sp/StringRange.java
@@ -3,7 +3,7 @@ package org.sp;
 public class StringRange {
      public void run(){
 
-         System.out.println("SFDFDSFSDF");
+         System.out.println("fsdjj");
      }
      public static String range(String input){
          if(input==null||input.endsWith("")){
